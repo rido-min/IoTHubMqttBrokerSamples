3e38cb691d495ca567631df0abf0f3ccb6a7bdcc
rido-min <rido-min@users.noreply.github.com>
2021-07-28 20:13:30 +0200

    publish

---
+++ publish/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Text;
+using System.Threading.Tasks;
+using AzureIoTHubDevices;
+using MQTTnet;
+using MQTTnet.Client;
+
+namespace publish
+{
+    class Program
+    {
+        static string cs = Environment.GetEnvironmentVariable("CS");
+
+        static async Task Main(string[] args)
+        {
+            var mqttClient = await IoTHubClient.CreateFromConnectionString(cs);
+
+            var mqttMessage = new MqttApplicationMessage()
+            {
+                Topic = "rido/one",
+                Payload = Encoding.UTF8.GetBytes("<message-payload>"),
+                QualityOfServiceLevel = 0
+            };
+
+            await mqttClient.PublishAsync(mqttMessage);
+
+            Console.WriteLine("Message Published");
+            Console.ReadLine();
+        }
+    }
+}
